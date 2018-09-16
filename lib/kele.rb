0a902ade4d48168a133355cec1e8d5e35b527723
David Buser <busdav@outlook.com>
2018-09-16 16:25:48 +0700

    Roadmaps and checkpoints: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,10 +1,12 @@
 require 'httparty'
 require 'json'
 require 'pp'
+require './lib/roadmap'
 
 class Kele
   include HTTParty
   base_uri 'https://www.bloc.io/api/v1'
+  include Roadmap
 
   def initialize(e, p)
     @auth = {email: e, password: p}
@@ -15,12 +17,12 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get("/users/me", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get('/mentors/#{mentor_id}/student_availability', headers: { "authorization" => @auth_token })
+    response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 end
---
+++ lib/roadmap.rb
@@ -0,0 +1,12 @@
+
+module Roadmap
+  def get_roadmap(chain_id)
+    response = self.class.get("/roadmaps/#{chain_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+end
